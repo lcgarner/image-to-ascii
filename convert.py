459ba54a7f9df260abb08e5217baab764bf4069f
Lily Garner <lgarner@smith.edu>
2024-03-17 00:01:35 +0100

    Add comments to convert.py

--- convert.py
+++ convert.py
@@ -1,21 +1,26 @@
 import sys
 from PIL import Image
 
+# ASCII characters, building blocks of the output 
 ASCII_CHARS = "@%#*+=-:. "
 
+# Load image from path
 def load_image(image_path):
     return Image.open(image_path)
 
+# Convert image to grayscale
 def grayscale_image(image):
     return image.convert("L")
 
+# Resize image according to a new width because character sizes are funky
 def resize_image(image, new_width=100):
     width, height = image.size
-    ratio = height / width / 1.65
+    ratio = height / width / 1.65 # Adjust as needed
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
     return resized_image
 
+# Map each pixel to an ASCII char
 def map_pixels_to_ascii(image, range_width=25):
     pixels = image.getdata()
     ascii_str = ''
@@ -23,6 +28,7 @@ def map_pixels_to_ascii(image, range_width=25):
         ascii_str += ASCII_CHARS[pixel_value // range_width]
     return ascii_str
 
+# Convert image to ASCII art
 def convert_image_to_ascii(image, new_width=100):
     image = resize_image(image, new_width)
     image = grayscale_image(image)
@@ -32,16 +38,18 @@ def convert_image_to_ascii(image, new_width=100):
     ascii_str_len = len(ascii_str)
     ascii_img=""
 
+    # Split the string based on the width of the image
     for i in range(0, ascii_str_len, img_width):
         ascii_img += ascii_str[i:i+img_width] + "\n"
 
     return ascii_img
 
+# Main function to run the program
 def main():
     if len(sys.argv) > 1:
-        image_path = sys.argv[1]  
+        image_path = sys.argv[1]  # Get image path from command line arguments
     else:
-        image_path = input("Enter the path to the image file: ")  
+        image_path = input("Enter the path to the image file: ")  # Prompt user to enter the path
 
     try:
         width = int(input("Enter the desired width of the ASCII Art (e.g., 100): "))
